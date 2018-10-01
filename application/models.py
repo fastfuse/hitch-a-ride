fea2b09ee9d504390545e4dfcd33c6fc5f893d92
Vasyl Voityk <fastfuse@vvoityk.local>
2018-10-01 16:59:51 +0300

    wip

--- application/models.py
+++ application/models.py
@@ -33,6 +33,10 @@ class User(db.Model, BaseMixin, UserMixin):
     last_name = db.Column('last_name', db.String(20))
     password_hash = db.Column('password', db.String)
 
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    role = db.relationship('Role', backref=db.backref('users', lazy='dynamic'))
+
     def hash_password(self, password):
         self.password_hash = generate_password_hash(password)
 
@@ -41,3 +45,14 @@ class User(db.Model, BaseMixin, UserMixin):
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
+
+
+class Role(db.Model, BaseMixin):
+    """
+    Model represents role
+    """
+
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    role = db.Column('role', db.String(20))
--- config.py
+++ config.py
@@ -6,6 +6,11 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY',
                                 '\x86Zh\x91\xd4\x05]\x1d\x950\xe5\xa3\x01UC\x08)\xc44_\xc1\xc7\xe6~')
 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
+                                             "postgresql://hitch:admin@postgres:5432/hitch_db")
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
