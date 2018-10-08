bd2894d96a5b06d9418cb618da443cfa70c6f099
Vasyl Voityk <fastfuse@vvoityk.local>
2018-10-08 16:29:33 +0300

    admin security started

--- application/__init__.py
+++ application/__init__.py
@@ -2,20 +2,20 @@ import os
 
 from flask import Flask
 from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 from flask_jwt_extended import JWTManager
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+
+# from application.admin.views import SecureAdminIndexView
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 # Login
 login = LoginManager(app)
-login.login_view = 'login'
-
-# Admin
-admin = Admin(app)
+# login.login_view = 'login'
 
 # Database
 db = SQLAlchemy(app)
@@ -39,6 +39,18 @@ def make_shell_context():
 
 
 from application import models
+
+from .admin.views import SecureAdminIndexView, SecureModelView
+
+# Admin
+admin = Admin(app, name="Hitch A Ride Admin",
+              index_view=SecureAdminIndexView(),
+              base_template='admin/custom_nav.html')
+
+admin.add_view(SecureModelView(models.User, db.session))
+admin.add_view(SecureModelView(models.Role, db.session))
+admin.add_view(SecureModelView(models.Trip, db.session))
+admin.add_view(SecureModelView(models.TokenBlacklist, db.session))
 
 from .admin import admin_blueprint
 from .auth import auth_blueprint
--- application/admin/views.py
+++ application/admin/views.py
@@ -2,9 +2,23 @@
 Admin views.
 """
 
+from flask import Flask, url_for, redirect, render_template, request
+
 from flask_admin.contrib.sqla import ModelView
 
-from application import admin, models, db
+from flask_admin import AdminIndexView, expose, helpers
+from flask_login import current_user, login_user, logout_user
+from werkzeug.security import check_password_hash
+
+from application import admin, models, db, login
+
+from wtforms import form, fields, validators
+
+
+# Create user loader function
+@login.user_loader
+def load_user(user_id):
+    return db.session.query(models.User).get(int(user_id))
 
 
 # TODO: secure
@@ -15,10 +29,76 @@ class SecureModelView(ModelView):
     Make view accessible only for admin.
     """
 
-    column_list = ['email', 'first_name', 'last_name', 'role', 'registered_on']
+    # column_list = ['email', 'first_name', 'last_name', 'role', 'registered_on']
+
+    # exclude password hash from admin view
+    column_exclude_list = ['password_hash', ]
+
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.role.name == 'Admin'
 
 
-admin.add_view(SecureModelView(models.User, db.session))
-admin.add_view(ModelView(models.Role, db.session))
-admin.add_view(ModelView(models.Trip, db.session))
-admin.add_view(ModelView(models.TokenBlacklist, db.session))
+# admin_ext.add_view(SecureModelView(models.User, db.session))
+# admin_ext.add_view(ModelView(models.Role, db.session))
+# admin_ext.add_view(ModelView(models.Trip, db.session))
+# admin_ext.add_view(ModelView(models.TokenBlacklist, db.session))
+
+
+# ======================================================================
+
+class LoginForm(form.Form):
+    email = fields.StringField(validators=[validators.required()])
+    password = fields.PasswordField(validators=[validators.required()])
+
+    def validate_email(self, field):
+        user = self.get_user()
+
+        if user is None:
+            raise validators.ValidationError('Invalid email')
+
+        if not user.check_password(self.password.data):
+            raise validators.ValidationError('Invalid password')
+
+    def validate_password(self, field):
+        if not self.user:
+            return
+
+        if not self.user.check_password(self.password.data):
+            raise validators.ValidationError('Invalid password')
+
+    def get_user(self):
+        self.user = db.session.query(models.User).filter_by(email=self.email.data).first()
+
+        return self.user
+
+
+class SecureAdminIndexView(AdminIndexView):
+
+    @expose('/')
+    def index(self):
+        if not current_user.is_authenticated:
+            return redirect(url_for('.login_view'))
+
+        return super(SecureAdminIndexView, self).index()
+
+    @expose('/login', methods=('GET', 'POST'))
+    def login_view(self):
+        # handle user login
+        login_form = LoginForm(request.form)
+
+        if helpers.validate_form_on_submit(login_form):
+            user = login_form.get_user()
+            login_user(user)
+
+        if current_user.is_authenticated:
+            return redirect(url_for('.index'))
+
+        self._template_args['form'] = login_form
+
+        return super(SecureAdminIndexView, self).index()
+
+    @expose('/logout')
+    def logout_view(self):
+        logout_user()
+
+        return redirect(url_for('.index'))
